9096f923d352644caa1351788dd51076905faf9f
Ron Lauren Hombre <ronlauren@hombre.asia>
2025-12-09 05:06:19 +0800

    Fix #1

--- src/main/kotlin/asia/hombre/neorust/task/RunBinary.kt
+++ src/main/kotlin/asia/hombre/neorust/task/RunBinary.kt
@@ -30,6 +30,7 @@ import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
 import javax.inject.Inject
@@ -55,6 +56,7 @@ abstract class RunBinary @Inject constructor(): DefaultTask() {
     @get:Input
     abstract val buildProfile: Property<String>
     @get:Input
+    @get:Optional
     abstract val workingDir: Property<String>
     @get:Input
     abstract val arguments: ListProperty<String>
